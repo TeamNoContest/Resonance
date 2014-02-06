56b2feb1c778b2a4df9aeeb960e8cb5de8cf57c3
Retl <carlis.moore@gmail.com>
2014-02-06 16:20:13 -0500

    Unity Project Basic Folder Framework Update
    Just setup a github just in case our repository thing doesn't work out
    too great. Also made a Unity project to lay out a folder structure
    thinger with Unity 4.3.3 (I'll have to upgrade to 4.3.4 soon) and then
    made my personal time tracking sheet thingy.

---
+++ UnityProject/Assets/RR_Classes/GenericUnitBehavior.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GenericUnitBehavior : MonoBehaviour
+{
+
+	//Property Instantiation Section
+	public string AllyTag { get; set; }
+
+	private float GatheringRate { get; set; }
+
+	private float AttackRate { get; set; }
+
+	private float RateModifer { get; set; }
+
+	private float MovementSpeed { get; set; }
+
+	private float ResourceCapacity { get; set; }
+
+	private float ResourceLoad { get; set; }
+
+	private float Integrity { get; set; } //This is kinda like a healthbar. This shouldn't be very important in our prototype's mode of play.;
+
+	//Event Methods - Start
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	//Event Methods - End
+
+	//Accessor Methods - Start
+
+	//Accessor Methods - End
+
+	//Mutator Methods - Start
+
+	//Mutator Methods - End
+}
