22a4f95a6fee263385d47e33c5d13bc0c7086659
Retl <carlis.moore@gmail.com>
2014-02-12 17:16:01 -0500

    Added Models and Camera Script
    Added a new C# script to make the camera follow a designated player
    ship. (Though maybe this should be done by the game controller?)
    Imported the models and put them into the test scene as entities to play
    around with. Added a RR_Models and RR_Materials folder for models and
    materials. Added a particle effect and sphere child to the Resonator
    entity to see how it would look.

---
+++ UnityProject/Assets/RR_Classes/CameraPlayerTracker.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraPlayerTracker : MonoBehaviour {
+
+	//Public properties to be set in inspector.
+	public GameObject thePlayer;
+	public float maximumPlayerCameraDistance;
+	public float cameraChaseSpeed;
+	public float currentDistance; //This exists soley to make it easier to watch in the inspector. - Moore
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (thePlayer != null)
+		{
+
+			currentDistance = Vector3.Distance(transform.position, thePlayer.transform.position);
+			
+			if (currentDistance > maximumPlayerCameraDistance) 
+			{
+				//Vector3.MoveTowards(transform.position, new Vector3(thePlayer.transform.position.x, transform.position.y, thePlayer.transform.position.z ), cameraChaseSpeed); //Note, this doesn't change the elevation of the GameObject using this script. - Moore
+				transform.position = Vector3.Lerp(transform.position, new Vector3(thePlayer.transform.position.x, transform.position.y, thePlayer.transform.position.z ), cameraChaseSpeed / 100);
+			}
+		}
+
+		transform.LookAt(thePlayer.transform.position);
+	
+	}
+}
