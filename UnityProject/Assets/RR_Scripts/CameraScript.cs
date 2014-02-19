4d410bb401e4289a13ca28c11cea84dcd28d96be
Jared Cerbin <JDCAce@gmail.com>
2014-02-19 18:23:47 -0500

    CameraScript and Skybox

---
+++ UnityProject/Assets/RR_Scripts/CameraScript.cs
@@ -0,0 +1,58 @@
+﻿// CameraScript.cs
+//
+// Q and E rotate camera. 9 and 0 zoom out and in. This will change when we use an Xbox controller:
+// Holding the left shoulder button will put the player in "camera mode." While it's held, the
+// player can rotate the camera left and right by moving the right analog stick left and right, and he can
+// zoom out and in by moving the right analog stick down and up. Releasing the left shoulder button will
+// make the player leave "camera mode."
+
+using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour
+{
+	public const int MIN_ZOOM_DISTANCE = 80;
+	public const int MAX_ZOOM_DISTANCE = 200;
+	public Transform target;
+	public float distance;
+	public float lift;
+	public float xPosition;
+
+	void Start()
+	{
+	}
+
+	void Update()
+	{
+		//transform.position = target.position + new Vector3(xPosition, lift, distance);
+		transform.LookAt(target);
+
+		if(Input.GetKey(KeyCode.Q))
+		{
+			transform.RotateAround(target.position, Vector3.up, 40 * Time.deltaTime);
+		}
+		else if(Input.GetKey(KeyCode.E))
+		{
+			transform.RotateAround(target.position, -Vector3.up, 40 * Time.deltaTime);
+		}
+
+		if(Input.GetKey(KeyCode.Alpha9))
+		{
+			Debug.Log(Vector3.Distance(transform.position, target.position));
+
+			if(Vector3.Distance(transform.position, target.position) <= MAX_ZOOM_DISTANCE)
+			{
+				transform.position += transform.forward * -1;
+			}
+		}
+		else if(Input.GetKey(KeyCode.Alpha0))
+		{
+			Debug.Log(Vector3.Distance(transform.position, target.position));
+
+			if(Vector3.Distance(transform.position, target.position) >= MIN_ZOOM_DISTANCE)
+			{
+				transform.position += transform.forward;
+			}
+		}
+	}
+}
---
+++ UnityProject/Assets/RR_Scripts/PlayerScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour
+{
+	private bool cameraMode = false;
+	void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.C))
+		{
+			cameraMode = true;
+		}
+		else if(Input.GetKeyUp(KeyCode.C))
+		{
+			cameraMode = false;
+		}
+
+		if(!cameraMode)
+		{
+			if(Input.GetAxis("Vertical") < 0)
+			{
+				transform.position += transform.forward;
+			}
+			else if(Input.GetAxis("Vertical") > 0)
+			{
+				transform.position += transform.forward * -1;
+			}
+
+			if(Input.GetAxis("Horizontal") < 0)
+			{
+				transform.position += transform.right;
+			}
+			else if(Input.GetAxis("Horizontal") > 0)
+			{
+				transform.position += transform.right * -1;
+			}
+		}
+	}
+}
