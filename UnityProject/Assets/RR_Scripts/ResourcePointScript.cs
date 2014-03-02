505162065f28180ecbec4519dfce67f964fa5049
Jared Cerbin <JDCAce@gmail.com>
2014-03-02 16:36:08 -0500

    Added ResourcePointScript.cs
    ResourcePointScript.cs adds functionality to decrease resources from a
    resource point. It listens for events from units, then decreases its
    resource count by an amount determined by all the "attached" units'
    gather rates. I have not implemented the event system yet, so you won't
    see much when you play the game. Also added a GUI Label above each
    resource point that displays how much resource it has left.

---
+++ UnityProject/Assets/RR_Scripts/ResourcePointScript.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResourcePointScript : MonoBehaviour
+{
+	int resourceCount;
+	int resourcesLostPerSecond;
+	int unitsAcquired;
+	
+	void Start()
+	{
+		resourceCount = 1000;
+		resourcesLostPerSecond = 0;
+		unitsAcquired = 0;
+	}
+	
+	void Update()
+	{
+		if(resourceCount <= 0)
+		{
+			Destroy(gameObject);
+		}
+	}
+	
+	void OnEnable()
+	{
+		InvokeRepeating("SubtractResource", 0f, 1f);
+		
+		// Subscribe to Attach event with AquireUnit
+		// Subscribe to Release event with ReleaseUnit
+	}
+	
+	void OnDisable()
+	{
+		// Unsubscribe from all events
+	}
+	
+	void AcquireUnit(int unitGatherRate)
+	{
+		unitsAcquired++;
+		resourcesLostPerSecond += unitGatherRate;
+	}
+	
+	void ReleaseUnit(int unitGatherRate)
+	{
+		unitsAcquired--;
+		resourcesLostPerSecond -= unitGatherRate;
+	}
+	
+	void SubtractResource()
+	{
+		resourceCount -= resourcesLostPerSecond;
+	}
+	
+	void OnGUI()
+	{
+		Vector2 labelPos = Camera.main.WorldToScreenPoint(transform.position);
+		GUI.Label(new Rect(labelPos.x, Screen.height - labelPos.y - 30, 50, 20), resourceCount.ToString());
+	}
+}
