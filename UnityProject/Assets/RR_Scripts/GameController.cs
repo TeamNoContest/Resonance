e37f86a200c2452cfff6be4b8b9444eedc01ea09
Retl <carlis.moore@gmail.com>
2014-03-09 22:44:21 -0400

    Units Collect properly. Resource Points Respawn.
    Added playing area, made resource nodes respawn in playing area. | 6:46
    PM Defect - 'resource' not in scope in several new methods - I forgot to
    initialize the variable type. | Tried to use 'this' from a script
    instead of refering to the script's container gameObject. | 7:10PM Logic
    Error - Accidentally tried to make objects move when too close to the
    player instead of too far. | 7:18PM - Initialized a lot of variables
    without assigning values to them, causing everything else to break. |
    7:29 - Spent 10 minutes trying to find and fix an error where
    'flytowardsgameobject' was calling the IsWithinRange instead of the
    negation. | 7:33PM - Logic Error - Resource Draw Speed is absurdly high.
    Unit movement speed is absurdly low. | Protection Level Error when
    trying to access GenericUnitBehavior's accessors from the
    GameController.

--- UnityProject/Assets/RR_Scripts/GameController.cs
+++ UnityProject/Assets/RR_Scripts/GameController.cs
@@ -17,6 +17,10 @@ public class GameController : MonoBehaviour
 	int unitCount;			// Current number of units under player control
 	int unitCap;			// Maximum number of units available to purchase
 
+	//I realize I shouldn't directly manipulate your code, but this is done to have the always up-to-date value from the player/mothership. - Moore
+	GameObject thePlayer;
+	GenericUnitBehavior thePlayerScript;
+
 	// Use this for initialization
 	void Start()
 	{
@@ -25,18 +29,31 @@ public class GameController : MonoBehaviour
 		resourceGoal = 1000;
 		timeCurrent = 0f;
 		timeGoal = 3600f;	// one hour time limit
-		interestRate = 0.05f;
+		interestRate = 0.01f; // This basically means a bonus 1 point per second for every 200, or - Moore
 		interestTime = 5f;
 		unitCount = 0;
 		unitCap = 10;
 
-		InvokeRepeating("AwardInterest", interestTime, interestTime);
+		//InvokeRepeating("AwardInterest", interestTime, interestTime); // Sorry for tweaking this without asking first. Gonna use Time.deltaTime for continuous intrest accruement. - Moore
+
+		//Initializing the values that retlate this to the other script. 
+		//This is not the best OOP because of the forced coupling. If you have an idea for how to keep the functionality and reduce this coupling, let me know. - Moore
+		thePlayer = GameObject.FindGameObjectWithTag("Player");
+		thePlayerScript = thePlayer.GetComponent<GenericUnitBehavior>();
+		resourceCurrent = (int)thePlayerScript.ResourceLoad;
+
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
 		// Listen for Unit Increase/Decrease event.
+
+		//Keeping the value up to date with respect to the player - Moore.
+		if (thePlayerScript != null)
+		{
+			resourceCurrent = (int)thePlayerScript.ResourceLoad;
+		}
 	}
 
 	void LateUpdate()
@@ -81,5 +98,10 @@ public class GameController : MonoBehaviour
 	{
 		float[] temp = {timeCurrent, timeGoal};
 		return temp;
+	}
+
+	public float GetInterestRate() //ACCESSOR: Added this so I can check it in the Units. - Moore
+	{
+		return interestRate;
 	}
 }
