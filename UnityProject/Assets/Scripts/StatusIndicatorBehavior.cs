0f09139e3751d3108986e0383cce4b66bd82e192
Retl <carlis.moore@gmail.com>
2014-03-25 16:14:38 -0400

    Added status indicator circles to our unit prefabs.
    Added status indicator circles to the unit prefabs, as well as a script
    to manipulate the unit prefabs and textures to use to color/fill in the
    materials.
    
    Defects: There was a defect where the material references weren't being
    properly set in the prefabs and would instead turn into a bright magenta
    placeholder. Fixed.
    
    There were a number of defects with BroadcastMessage where the messages
    were not received because the messages broadcast did not match the
    actual name of the methods intended to be called. Fixed.

---
+++ UnityProject/Assets/Scripts/StatusIndicatorBehavior.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StatusIndicatorBehavior : MonoBehaviour
+{
+
+    public Material matRed;
+    public Material matNone;
+    public Material matBlue;
+    public Material matYellow;
+    public Material matPurple;
+    public Material matWhite;
+
+    public bool gotMessage = false;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+	
+    // Update is called once per frame
+    void Update()
+    {
+	
+    }
+
+    //And this is all of those useful utility/modifier methods.
+    public void SetMatieralRed()
+    {
+        renderer.material = matRed;
+    }
+	
+    public void SetMatieralNone()
+    {
+        renderer.material = matNone;
+    }
+
+    public void SetMatieralBlue()
+    {
+        renderer.material = matBlue;
+    }
+	
+    public void SetMatieralYellow()
+    {
+        renderer.material = matYellow;
+    }
+
+    public void SetMatieralPurple()
+    {
+        renderer.material = matPurple;
+    }
+	
+    public void SetMatieralWhite()
+    {
+        renderer.material = matWhite;
+    }
+
+    public void getMessage()
+    {
+        gotMessage = true;
+    }
+}
