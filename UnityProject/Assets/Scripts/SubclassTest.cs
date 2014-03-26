766f2e9b4f7c7a96542e8954f2c26e8d624d1596
Jared Cerbin <JDCAce@gmail.com>
2014-03-26 14:45:25 -0400

    Subclassing Test
    Contains SubclassTest.cs and SuperclassTest.cs.

---
+++ UnityProject/Assets/Scripts/SubclassTest.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SubclassTest : SuperclassTest
+{
+	void Update()
+	{
+		base.Update();
+		print("Subclass: " + base.number/2);
+	}
+}
---
+++ UnityProject/Assets/Scripts/SuperclassTest.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SuperclassTest : MonoBehaviour
+{
+	protected internal int number;
+	
+	protected void Start()
+	{
+		number = 0;
+	}
+	
+	protected void Update()
+	{
+		print("Superclass: " + number++);
+	}
+}
