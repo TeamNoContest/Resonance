d128ccbf7284c911e3bc83ad5183f961609d6105
Retl <carlis.moore@gmail.com>
2014-04-07 23:09:47 -0400

    Added Alpha Unit Behavior file.

---
+++ UnityProject/Assets/Scripts/UnitBehaviors/AlphaUnitBehavior.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlphaUnitBehavior : MonoBehaviour {
+
+	
+	GenericUnitBehavior gun;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		gun = GetComponent<GenericUnitBehavior> ();
+		
+		
+	}
+	
+	protected void SetStartValues()
+	{
+		if (gun != null) 
+		{
+			gun.GatheringRate = 10.0f;
+			gun.MovementSpeed = 15.0f;
+			gun.ResourceCapacity = -1f;
+			gun.ResourceLoad = 3000.0f; 
+			gun.Integrity = 200.0f;
+		}
+	}
+
+}
