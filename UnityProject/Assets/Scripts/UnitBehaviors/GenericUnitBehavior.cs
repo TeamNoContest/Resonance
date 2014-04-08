22bec3cf402f0d466f09aa045945a79b05c9d8a5
Retl <carlis.moore@gmail.com>
2014-04-07 22:18:37 -0400

    Revert "Revert "Moved utilities to a dedicated library.""
    This reverts commit 4d33ac5bd05f92f31881d77f1feb90922bc59386.

--- UnityProject/Assets/Scripts/UnitBehaviors/GenericUnitBehavior.cs
+++ UnityProject/Assets/Scripts/UnitBehaviors/GenericUnitBehavior.cs
@@ -8,11 +8,10 @@ public class GenericUnitBehavior : MonoBehaviour
     public enum State
     {
         Dummy,
-        ChaseTarget,
         FollowPlayer,
         GatherNearestResourcePoint,
-        ReturnToBase,
         Stay,
+		ReturnToBase,
         DropoffAtFreighter,
         Attack,
         
@@ -54,7 +53,7 @@ public class GenericUnitBehavior : MonoBehaviour
     protected float interestRate { get; set; }// This one is taken from the GameController and updated with each update. - Moore
 
     protected Vector3 startPosition;
-    protected const float distanceTreshold = 20.0f;
+    protected const float distanceThreshold = 20.0f;
     private bool isPaused;
 	public bool isSelected;
     public GameObject target;
@@ -84,11 +83,6 @@ public class GenericUnitBehavior : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        //BroadcastMessage("getMessage");
-        //if (shipType == null)
-        //{
-        //    shipType = ShipType.Node;
-        //} //Defaults to Node only if a type is not selected.
         if (shipType == ShipType.Alpha)
         {
             theGameControllerScript = theGameController.GetComponent<GameController>();
@@ -98,8 +92,6 @@ public class GenericUnitBehavior : MonoBehaviour
         // I commented out the line above and took this out of the 'if' statement.
         // I need access to this script so I can detect if the game is paused.
         // (Refer to the OnGUI method.)
-        //theGameController = GameObject.Find("Game Controller Prop");
-        //theGameControllerScript = theGameController.GetComponent<GameController>();
 
         playingArea = GameObject.FindGameObjectWithTag("PlayingArea");
 
@@ -168,31 +160,17 @@ public class GenericUnitBehavior : MonoBehaviour
                 ApplyBrakes();
                 break;
 
-            case State.ChaseTarget:
-            //Pick the cloest target with the appropriate tag and move towards it. - Moore
-                target = FindClosestGameObjectWithTag("Target");
-                FlyTowardsGameObject(target);
-
-
-            //If too far from the start position, teleport back to start.
-                if (Vector3.Distance(startPosition, transform.position) > 100)
-                { 
-                    transform.position = startPosition;
-                }
-
-                break;
-
             case State.FollowPlayer:
-                target = FindClosestGameObjectWithTag("Player");
+                target = LibRevel.FindClosestGameObjectWithTag(gameObject, "Player");
                 FlyTowardsGameObjectWithSmartBraking(target);
                 break;
 
             case State.GatherNearestResourcePoint:
             //Search for the nearest resource point. - Moore
-                target = FindClosestGameObjectWithTag("ResourcePoint");
+                target = LibRevel.FindClosestGameObjectWithTag(gameObject, "ResourcePoint");
 
             //If close enough, draw resources from it. If not, then fly closer. - Moore
-                if (IsWithinDistanceThreshold(target))
+                if (LibRevel.IsWithinDistanceThreshold(gameObject, target, distanceThreshold))
                 {
                     ApplyBrakes();
                     GatherResourcesFromSource(target);
@@ -207,19 +185,17 @@ public class GenericUnitBehavior : MonoBehaviour
                     if (shipType == ShipType.Freighter)
                     {
                         state = State.ReturnToBase;
-                        //UpdateStatusIndicator(); DELETEME
                     } else
                     {
                         state = State.DropoffAtFreighter;
-                        //UpdateStatusIndicator(); DELETEME
                     }
                 }
 
                 break;
 
             case State.ReturnToBase:
-                target = FindClosestGameObjectWithTag("Player"); //This had been planned to be a separate ship, but now it refers to the player. - Moore
-                if (IsWithinDistanceThreshold(target))
+                target = LibRevel.FindClosestGameObjectWithTag(gameObject, "Player"); //This had been planned to be a separate ship, but now it refers to the player. - Moore
+                if (LibRevel.IsWithinDistanceThreshold(gameObject, target, distanceThreshold))
                 {
                     ApplyBrakes();
                     TransferResourcesToSource(target); //If you're close enough to the player, drop off your resources until you're empty. - Moore
@@ -231,8 +207,7 @@ public class GenericUnitBehavior : MonoBehaviour
             //If no more resources to deposit, go back to the default behavior of following the player.
                 if (ResourceLoad <= 0.0f)
                 {
-				state = State.FollowPlayer;
-                    //UpdateStatusIndicator(); DELETEME
+					state = State.FollowPlayer;
                 }
             
                 break;
@@ -244,8 +219,8 @@ public class GenericUnitBehavior : MonoBehaviour
             else
             {
             
-                target = FindClosestGameObjectWithTag("Freighter");
-                altTarget = FindClosestGameObjectWithTag("Player");
+                target = LibRevel.FindClosestGameObjectWithTag(gameObject, "Freighter");
+                altTarget = LibRevel.FindClosestGameObjectWithTag(gameObject, "Player");
 
                 if (target != null && altTarget != null)
                 {
@@ -260,7 +235,7 @@ public class GenericUnitBehavior : MonoBehaviour
 					target = altTarget;
 				}
 
-				if (IsWithinDistanceThreshold(target))
+				if (LibRevel.IsWithinDistanceThreshold(gameObject, target, distanceThreshold))
 				{
 					ApplyBrakes();
 					TransferResourcesToSource(target); //If you're close enough to the player, drop off your resources until you're empty. - Moore
@@ -273,8 +248,6 @@ public class GenericUnitBehavior : MonoBehaviour
 			if (ResourceLoad <= 0.0f)
 			{
 				state = State.FollowPlayer;
-				//UpdateStatusIndicator(); DELETEME
-				
 			}
                 break;
 
@@ -305,8 +278,6 @@ public class GenericUnitBehavior : MonoBehaviour
                     //Only the player gains interest and communicates with the GameController.
                     if (theGameControllerScript != null)
                     {
-                        //Update the interest from the GameController.
-                        //interestRate = theGameControllerScript.InterestRate;
                         //Bank the interest from existing resources.
                         ResourceLoad += ResourceLoad * interestRate * Time.deltaTime;
                     }
@@ -319,7 +290,7 @@ public class GenericUnitBehavior : MonoBehaviour
                     {
                         //Destroy(gameObject);
                         ResourceLoad += Random.Range(200, 2000);
-                        transform.position = RandomVector3InRange(playingArea.renderer.bounds.min.x, playingArea.renderer.bounds.max.x, 0, 0, playingArea.renderer.bounds.min.z, playingArea.renderer.bounds.max.z);
+                        transform.position = LibRevel.RandomVector3InRange(playingArea.renderer.bounds.min.x, playingArea.renderer.bounds.max.x, 0, 0, playingArea.renderer.bounds.min.z, playingArea.renderer.bounds.max.z);
 
                     }
                     break;
@@ -351,13 +322,12 @@ public class GenericUnitBehavior : MonoBehaviour
         if (usScript != null)
         {
             //Then we add ourselves to the selection and start following the player. But only if we're not a player.
-            if (shipType != ShipType.Alpha /* && usScript.iCanHasSelected*/)
+            if (shipType != ShipType.Alpha)
             {
                 CommandHandler.OnCommand += HandleCommandEvent;
 				UnitSelection.OnDeselect += HandleDeselectEvent;
 				isSelected = true;
                 state = State.FollowPlayer;
-                //UpdateStatusIndicator(); //DELETEME
             }
         }
     }
@@ -418,18 +388,6 @@ public class GenericUnitBehavior : MonoBehaviour
     {
 
         //Precondition: You should only call this once per calling object per update.
-
-        /*
-        result = 0.0f;
-        GenericUnitBehavior gubScript = targetObject.GetComponent<GenericUnitBehavior>();
-        if (gubScript != null)
-        {
-        result = gubScript.GatherResourcesFromSource(this);
-        }
-        return result;
-        // This version is a redirection helper mathod to swap arguments from GatherResourcesFromSource. DELETEME - Moore.
-         */
-
 
         //Check how many resources the target wants to take.
         float result = 0.0f;
@@ -497,12 +455,6 @@ public class GenericUnitBehavior : MonoBehaviour
                 case State.DropoffAtFreighter:
                     BroadcastMessage("SetMaterialYellow", true, SendMessageOptions.DontRequireReceiver); //The notes suggest this should be purple, but yellow is a bit easier to see the change.
                     break;
-
-            /*
-                case State.ReturnToBase:
-                    BroadcastMessage("SetMaterialYellow", true, SendMessageOptions.DontRequireReceiver); //The notes suggest this should be purple, but yellow is a bit easier to see the change.
-                    break;
-                    */
                 
                 default: 
                     BroadcastMessage("SetMaterialNone", true, SendMessageOptions.DontRequireReceiver);
@@ -559,32 +511,6 @@ public class GenericUnitBehavior : MonoBehaviour
 
     #region //Utility Methods - Start
 
-    protected void FlyTowardsGameObject(GameObject destination)
-    {
-        //Precondition: You should only be calling this once per caller per update. - Moore
-        if (destination != null)
-        {
-            //Make sure to check that the target is within a desirable distance. So there should be a maximum distance variable. - Moore
-            transform.LookAt(new Vector3(target.transform.position.x, transform.position.y, target.transform.position.z));
-            //Quaternion rotation = Quaternion.LookRotation((new Vector3(target.transform.position.x, transform.position.y, target.transform.position.z) - transform.position));
-            //transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * DAMPING);
-
-            if (IsNotWithinDistanceThreshold(destination))
-            { //4:23 Defect - Had the < intstead of >.
-                if (rigidbody == null)
-                {
-                    transform.position += (transform.forward * movementSpeed * rateModifier * Time.fixedDeltaTime);
-                } else
-                {               //Changing this ^^ to use rigidbodies and forces instead to allow for less weird overlapping stuff. - Moore.            
-                    rigidbody.freezeRotation = true;
-                    rigidbody.AddForce(-rigidbody.velocity);
-                    rigidbody.AddForce(transform.forward * movementSpeed * rateModifier * Time.fixedDeltaTime); //NOTE: We should only be using *this* version in a fixed update. - Moore.
-                    //rigidbody.velocity = transform.forward * movementSpeed * rateModifier * Time.deltaTime;         
-                }       
-            }
-        }
-    }
-
     protected void FlyTowardsGameObjectWithSmartBraking(GameObject destination)
     {
         //Description: This is very similar to the original FlyTowardsGameObject script, but is intended only for use with GameObjects that have rigid bodies. If it detects that its destination is within breaking distance it will start to slow down. - Moore.
@@ -592,7 +518,6 @@ public class GenericUnitBehavior : MonoBehaviour
         if (destination != null)
         {
             //Make sure to check that the target is within a desirable distance. So there should be a maximum distance variable. - Moore
-            //transform.LookAt(new Vector3(target.transform.position.x, transform.position.y, target.transform.position.z));
             Quaternion rotation = Quaternion.LookRotation((new Vector3(target.transform.position.x, transform.position.y, target.transform.position.z) - transform.position));
             transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * DAMPING);
 
@@ -604,7 +529,7 @@ public class GenericUnitBehavior : MonoBehaviour
 
                     rigidbody.freezeRotation = true;
                     rigidbody.AddForce(-rigidbody.velocity);
-                    rigidbody.AddForce(transform.forward * movementSpeed * rateModifier /** Time.fixedDeltaTime*/); //NOTE: We should only be using *this* version in a fixed update. - Moore.
+                    rigidbody.AddForce(transform.forward * movementSpeed * rateModifier); //NOTE: We should only be using *this* version in a fixed update. - Moore.
                 } else
                 {
                     print("The method \"FlyTowardsGameObjectWithSmartBraking\" was called on an object with no rigidbody. Did you mean to use \"FlyTowardsGameObject\"?");
@@ -616,32 +541,11 @@ public class GenericUnitBehavior : MonoBehaviour
         }
     }
 
-    protected bool IsWithinDistanceThreshold(GameObject destination) //TODO: Change this to factor in rigidbody.velocity so that it slows down before reaching destination. Alternatively, make a new method to do exactly that. - Moore
-    {
-        bool result = false;
-
-        if (Vector3.Distance(transform.position, destination.transform.position) > distanceTreshold)
-        { //If it is outside of the threshold...
-            result = false; //Return false. - Moore
-        } else
-        { // Otherwise, return true. - Moore
-            return true;
-        }
-
-        return result;
-    }
-
-    //Convenience Negation Wrapper Method for the above:
-    protected bool IsNotWithinDistanceThreshold(GameObject destination)
-    {
-        return !IsWithinDistanceThreshold(destination);
-    }
-
-    protected bool IsWithinBrakingDistance(GameObject destination) //TODO: Change this to factor in rigidbody.velocity so that it slows down before reaching destination. Alternatively, make a new method to do exactly that. - Moore
+    protected bool IsWithinBrakingDistance(GameObject destination)
     {
         bool result = false;
         
-        if (Vector3.Distance(transform.position, destination.transform.position) > (distanceTreshold + (rigidbody.velocity.magnitude /* * 2*/)) && rigidbody.velocity.magnitude > 0)
+        if (Vector3.Distance(transform.position, destination.transform.position) > (distanceThreshold + (rigidbody.velocity.magnitude /* * 2*/)) && rigidbody.velocity.magnitude > 0)
         { //If it is outside of the threshold...
             result = false; //Return false. - Moore
         } else
@@ -655,7 +559,7 @@ public class GenericUnitBehavior : MonoBehaviour
     //Convenience Negation Wrapper Method for the above:
     protected bool IsNotWithinBrakingDistance(GameObject destination)
     {
-        return !IsWithinDistanceThreshold(destination);
+		return !IsWithinBrakingDistance(destination);
     }
 
     protected void ApplyBrakes()
@@ -668,39 +572,6 @@ public class GenericUnitBehavior : MonoBehaviour
                 rigidbody.velocity = Vector3.zero;
             }
         }
-    }
-
-    protected GameObject FindClosestGameObjectWithTag(string tagToFind)
-    {
-        GameObject result = null;
-        GameObject[] allObjects = GameObject.FindGameObjectsWithTag(tagToFind);
-        
-        foreach (GameObject current in allObjects)
-        {
-            if (current != this.gameObject)
-            {
-                if (result == null)
-                {
-                    result = current;
-                } else
-                {
-                    if (Vector3.Distance(transform.position, result.transform.position) > Vector3.Distance(transform.position, current.transform.position))
-                    {
-                        result = current;
-                    }
-                }
-            }
-        }
-        return result;
-    }
-
-    protected Vector3 RandomVector3InRange(float xLow, float xHigh, float yLow, float yHigh, float zLow, float zHigh)
-    {
-        float tempX = Random.Range(xLow, xHigh);
-        float tempY = Random.Range(yLow, yHigh);
-        float tempZ = Random.Range(zLow, zHigh);
-
-        return new Vector3(tempX, tempY, tempZ);
     }
 
     // This is the class that handles the OnPause event, which is broadcast by GameController whenever the RunState changes.
